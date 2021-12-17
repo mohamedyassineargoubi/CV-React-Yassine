6156c634442c8b77ea47c0cb151339b666ffbc61
Yassine <56802832+mohamedyassineargoubi@users.noreply.github.com>
2021-12-17 17:20:09 +0100

    Update portfolioData.js

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -2,8 +2,8 @@ export const portfolioData = [
   {
     id: 1,
     name: ' CV ONLINE ',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt','fab fa-react'],
+    languages: ['javascript', 'react'],
+    languagesIcons: ['fab fa-js', 'fab fa-css3-alt', 'fab fa-react'],
     source: 'https://github.com/mohamedyassineargoubi/cv-react-yassine',
     info: 'Projet React dans lequel nous verrons comment réaliser un CV sous la forme d’une Single Page Application.',
     picture: './media/Cv_Project.JPG'
@@ -11,8 +11,8 @@ export const portfolioData = [
   {
     id: 2,
     name: 'CRUD FOR NOBO ',
-    languages: ['javascript','css','react'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt','fab fa-react'],
+    languages: ['javascript', 'css', 'react'],
+    languagesIcons: ['fab fa-js', 'fab fa-css3-alt', 'fab fa-react'],
     source: 'https://github.com/mohamedyassineargoubi/testTechniqueNobo',
     info: 'Projet React dans lequel nous verrons comment réaliser un CRUD sous la forme d’une Single Page Application pour la société NOBO.',
     picture: './media/Nobo_Project.JPG'
@@ -20,20 +20,29 @@ export const portfolioData = [
   {
     id: 3,
     name: 'WORLD PROJECT',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt','fab fa-react'],
+    languages: ['javascript', 'react'],
+    languagesIcons: ['fab fa-js', 'fab fa-css3-alt', 'fab fa-react'],
     source: 'https://github.com/mohamedyassineargoubi/World_React_Project',
     info: 'Projet React dans lequel nous verrons comment réaliser un Projet qui s’appel World sous la forme d’une Single Page Application qui contient tout les prérequis pour comprendre REACT.',
     picture: './media/World_Project.JPG'
   },
   {
     id: 4,
-    name: 'REDUX PROJECT',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt','fab fa-react'],
-    source: 'http://www.github.com',
+    name: 'POSTS AND USERS APP',
+    languages: ['javascript', 'react'],
+    languagesIcons: ['fab fa-js', 'fab fa-css3-alt', 'fab fa-react'],
+    source: 'https://github.com/mohamedyassineargoubi/Redux_Project_For_Posts-Users',
     info: 'Projet React dans lequel nous verrons comment réaliser un Projet Redux sous la forme d’une Single Page Application qui contient tout les prérequis pour comprendre Redux.',
     picture: './media/Redux_Project.JPG'
+  },
+  {
+    id: 5,
+    name: 'SHOPPING APP',
+    languages: ['javascript', 'react', 'css'],
+    languagesIcons: ['fab fa-js', 'fab fa-css3-alt', 'fab fa-react'],
+    source: 'https://github.com/mohamedyassineargoubi/Shopping-Website-React-Redux',
+    info: 'Projet React dans lequel nous verrons comment réaliser un Projet Redux sous la forme d’une Single Page Application qui contient tout les prérequis pour comprendre Redux.',
+    picture: './media/Shopping_Redux.PNG'
   },
 
 ]
